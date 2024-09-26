c073342963261dba47d7de1ea0075bd893bd1d86
Astrid-Lo <astridlo131@gmail.com>
2024-09-26 17:14:22 +0200

    Placing items
    ajouté un dossier Scenes et un objet 3D item_spots. cet objet servira a detecter les collisions pour que le joueur puisse placer des objets

---
+++ item_spot.gd
@@ -0,0 +1,14 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+#Takes an item out of the player's inventory and puts it back in its place if it's the right item
+func put_back_item():
+	#checks the player's inventory 
+	#if on the of the can be put here then it places the item in its place
+	#takes the item out of the player's inventory
+	#destroys itself
+	pass
