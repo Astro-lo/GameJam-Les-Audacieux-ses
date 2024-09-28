08e1e39940bfdcafe555665015169470d11aea87
Astrid-Lo <astridlo131@gmail.com>
2024-09-28 16:52:05 +0200

    reglé camera

--- scripts/CameraMan.gd
+++ scripts/CameraMan.gd
@@ -6,40 +6,45 @@ extends Node
 @export var triggerLeft : Area3D
 @export var triggerRight : Area3D
 
-@export var fireplace : Mesh
-@export var chimney : Mesh
-@export var ground : Mesh
-@export var frontBox : Mesh
+@export var fireplace : MeshInstance3D
+@export var chimney : MeshInstance3D
+@export var ground : MeshInstance3D
+@export var frontBox : MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	triggerLeft.area_entered.connect(entered_area_left);
-	triggerRight.area_entered.connect(entered_area_right);
-	pass # Replace with function body.
+	assert(triggerLeft)
+	assert(triggerRight)
+	
+	assert(cameraLeft)
+	assert(cameraRight)
+	
+	triggerLeft.body_entered.connect(entered_area_left);
+	triggerRight.body_entered.connect(entered_area_right);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _swapRoom(input: Mesh, leftSide: bool, screenmode: bool = false) -> void:
-	var m = input.surface_get_material(0)
+func _swapRoom(input: MeshInstance3D, leftSide: bool, screenmode: bool = false) -> void:
+	var m = input.get_active_material(0)
 	m.set("secondUVSet", leftSide)
 	m.set("roomSideLeft", leftSide)
 	m.set("screenMode", screenmode)
 
-func entered_area_left() ->void:
-	cameraLeft.make_current()
+func entered_area_left(node: Node3D) ->void:
+	cameraRight.make_current()
 	_swapRoom(fireplace, true)
 	_swapRoom(chimney, true)
 	_swapRoom(frontBox, true)
 	_swapRoom(ground, true, true)
-	pass
+	
 
-func entered_area_right() ->void:
-	cameraRight.make_current();
+func entered_area_right(node: Node3D) ->void:
+	cameraLeft.make_current();
 	_swapRoom(fireplace, false)
 	_swapRoom(chimney, false)
 	_swapRoom(frontBox, false)
 	_swapRoom(ground, false, true)
-	pass
+	
