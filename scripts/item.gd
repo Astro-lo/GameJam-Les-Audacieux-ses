ac721949a67edaefc0a7c0b6c8b03979bf45a5a7
Astrid-Lo <astridlo131@gmail.com>
2024-09-28 11:30:14 +0200

    .

--- scripts/item.gd
+++ scripts/item.gd
@@ -5,11 +5,14 @@ extends MeshInstance3D
 
 var player_inv = preload("res://Scenes/Inventory/inventory.tres")
 #When the player walks over the item it will be put into the player's inventory
-func pick_up():
-	#get the player's inventory and places itself here
+func pick_up(plr):
+	print(plr.inv.Item.size())
 	#start a function that highlights where it can be placed
 	#destroy itself
-	pass
 
 func add_to_inv(item):
-	
+	pass
+
+
+func _on_area_3d_body_entered(body: CharacterBody3D) -> void:
+	pick_up(body)
