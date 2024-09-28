c4d4f892f55691295ca2ef46cd287ef8a591f6c7
lea Nyu <dontCare>
2024-09-29 00:45:07 +0300

    deleting that little s script folder

--- scripts/inventory.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name Inv
-#the inventory of the player, it can store items as PackedScenes '.tscn' in it's array
-@export var Test: String
-@export var Items: Array[InvItem]
